19c64b83a48d2e4f621e09881f25d32958b669f2
Scott Lucas <scott@scotts-macbook.local>
2018-05-10 14:21:01 -0700

    Initial

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -1,6 +1,7 @@
 import Vapor
 
 /// Creates an instance of Application. This is called from main.swift in the run target.
+//Tutorial in use by Scott Lucas
 public func app(_ env: Environment) throws -> Application {
     var config = Config.default()
     var env = env
